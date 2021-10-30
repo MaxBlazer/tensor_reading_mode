727a5c0d6ea1c17d6b826f0020d7003d2e66f0b9
maxblazer <maxiblazer@gmail.com>
2021-10-30 22:33:54 +0700

    done

--- tensor_reading_mode/main.py
+++ tensor_reading_mode/main.py
@@ -1,5 +1,8 @@
+import re
+
 import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
+import argparse
 
 
 def wrap(text: str):
@@ -16,11 +19,59 @@ def wrap(text: str):
 
 
 def main():
-    url = 'https://lenta.ru/news/2021/10/29/memory/'
-    response = requests.get(url)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url')
+    args = parser.parse_args()
+
+    response = requests.get(args.url)
     page = response.text
-    soup = BeautifulSoup(page, features='html.parser')
-    print(wrap(soup.text))
+    soup = BeautifulSoup(page, features='html5lib')
+
+    soup = soup.body
+
+    if soup.main is not None:
+        soup = soup.main
+
+    if soup.article is not None:
+        soup = soup.article
+
+    ignored_tags = ['nav', 'script', 'style', 'meta', 'time', 'aside', 'header', 'footer']
+
+    ignored_classes = [
+        re.compile('.*header.*'),
+        re.compile('.*sidebar.*'),
+        re.compile('.*footer.*'),
+    ]
+
+    for tag_name in ignored_tags:
+        for tag in soup.find_all(tag_name):
+            tag.extract()
+
+    for class_ in ignored_classes:
+        for tag in soup.find_all(class_=class_):
+            tag.extract()
+
+    for tag in soup.find_all(style="display: none;"):
+        tag.extract()
+
+    def estimator(tag):
+        return len([child for child in tag.children if child.name == 'p'])
+
+    article = max(soup.find_all('div'), key=estimator)
+
+    allowed_children = ['p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+
+    for child in article.children:
+        if isinstance(child, Tag) and child.name not in allowed_children:
+            child.extract()
+
+    for a in article.find_all('a'):
+        a.replace_with(f"{a.text} [{a['href']}]")
+
+    result = '\n\n'.join(wrap(child.text) for child in article.children if isinstance(child, Tag))
+
+    with open('result.txt', 'w', encoding='utf-8') as file:
+        file.write(result)
 
 
 if __name__ == '__main__':
